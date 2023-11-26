ec4b18d89a8b06212787628fcfe12c55a40a97e3
Lubi Islam <lubaba.islam91@gmail.com>
2023-11-26 01:15:56 -0500

    second take

--- eqArrays.js
+++ eqArrays.js
@@ -7,12 +7,15 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(array1, array2) {
-  let i = 0;
-  if (array1[i] === array2[i]) {
-    console.log(`✅✅✅ true`);
-  } else {
-    console.log(`🛑🛑🛑 false`);
+  if (array1.length !== array2.length) {
+    return false;
   }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 eqArrays([1, 2, 3], [1, 2, 3]) // => true
@@ -21,4 +24,4 @@ eqArrays([1, 2, 3], [3, 2, 1]) // => false
 eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
 eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]));
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);

9569e738029bd72e6cd4b47496679a8dae18a260
Lubi Islam <lubaba.islam91@gmail.com>
2023-11-26 01:11:29 -0500

    first take

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  let i = 0;
+  if (array1[i] === array2[i]) {
+    console.log(`✅✅✅ true`);
+  } else {
+    console.log(`🛑🛑🛑 false`);
+  }
+};
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]));

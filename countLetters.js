b811ca7ba172e310174812366cd7773d03f104d0
Lubi Islam <lubaba.islam91@gmail.com>
2024-02-01 22:45:02 -0500

    second take

--- countLetters.js
+++ countLetters.js
@@ -24,3 +24,14 @@ function countLetters(allLetters) {
 
 const result = countLetters("lighthouse in the house");
 console.log(result);
+
+assertEqual(result['l'], 1);
+assertEqual(result['i'], 2);
+assertEqual(result['g'], 1);
+assertEqual(result['h'], 4);
+assertEqual(result['t'], 2);
+assertEqual(result['o'], 2);
+assertEqual(result['u'], 2);
+assertEqual(result['s'], 2);
+assertEqual(result['e'], 3);
+assertEqual(result['n'], 1);

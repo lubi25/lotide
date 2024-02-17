739c69e13f1a703c0792939d4ce393c9ce41c16e
Lubi Islam <lubaba.islam91@gmail.com>
2024-02-17 17:46:27 -0500

    second take

---
+++ eqObjects.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqObjects = (object1, object2) => {
+  const keys1 = Object.keys(object1).sort();
+  const keys2 = Object.keys(object2).sort();
+
+  if (keys1.length !== keys2.length) return false;
+
+  for (let i = 0; i < keys1.length; i++) {
+    const key = keys1[i];
+    const val1 = object1[key];
+    const val2 = object2[key];
+
+    if (typeof val1 !== typeof val2) return false;
+
+    if (typeof val1 === 'object' && typeof val2 === 'object') {
+      if (!eqObjects(val1, val2)) return false;
+    } else {
+      if (val1 !== val2) return false;
+    }
+  }
+
+  return true;
+};
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject= { size: "medium", color: "red" };
+eqObjects(shirtObject , anotherShirtObject); 
+
+const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
+eqObjects(shirtObject , longSleeveShirtObject);

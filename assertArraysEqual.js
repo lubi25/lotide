546c7ae262d5087e3bcf411b3f86483e2304352c
Lubi Islam <lubaba.islam91@gmail.com>
2024-02-17 18:50:48 -0500

    refactored

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -10,9 +10,4 @@ const assertArraysEqual = function(array1, array2) {
   return console.log("true");
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
-
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
-
 module.exports = assertArraysEqual;

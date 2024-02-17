7b559712eb4107351b153f36d6ae41c4c3d80a16
Lubi Islam <lubaba.islam91@gmail.com>
2024-02-17 18:36:56 -0500

    refactored

--- test/tailTest.js
+++ test/tailTest.js
@@ -5,3 +5,4 @@ const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2);
 assertEqual(result[0], "Lighthouse");
 assertEqual(result[1], "Labs");
+assertEqual(result[1], "Lighthouse");

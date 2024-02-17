bd0693f55e586afaae0922dc314536d3fea8a95d
Lubi Islam <lubaba.islam91@gmail.com>
2024-02-17 18:48:52 -0500

    refactored with module

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const assertArraysEqual = function(array1, array2) {
   if (array1.length !== array2.length) {
     return console.log("false");
@@ -22,3 +14,5 @@ assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
 assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
 
 assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
+
+module.exports = assertArraysEqual;

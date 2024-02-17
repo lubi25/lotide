02ab83dfc8441ffab7bdc8983b826e54a2255238
Lubi Islam <lubaba.islam91@gmail.com>
2024-02-17 18:10:01 -0500

    refactor w module

--- head.js
+++ head.js
@@ -1,14 +1,8 @@
+const assertEqual = require('./assertEqual');
+
 const head = function(array) {
   return array[0];
 }
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");

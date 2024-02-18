9806573ce473bc86d125557d868cabb66cadf372
Lubi Islam <lubaba.islam91@gmail.com>
2024-02-17 22:09:27 -0500

    restored to original

--- reverse.js
+++ reverse.js
@@ -1,16 +1,8 @@
-const reverse = function(str) {
+const args = process.argv.slice(2);
+args.forEach(str => {
   let reversed = '';
   for (let i = str.length - 1; i >= 0; i--) {
     reversed += str[i];
   }
-  return reversed;
-};
-
-if (require.main === module) {
-  const args = process.argv.slice(2);
-  args.forEach(str => {
-    console.log(reverse(str));
-  });
-}
-
-module.exports = reverse;
+  console.log(reversed);
+});

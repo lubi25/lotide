f92d24ce98aaa4ec4f5fce8a6ab9b0edfcff0a46
Lubi Islam <lubaba.islam91@gmail.com>
2024-02-17 21:04:40 -0500

    removed console.log

--- middle.js
+++ middle.js
@@ -1,13 +1,13 @@
 const middle = function(arr) {
   const length = arr.length;
   if (length <= 2) {
-    return console.log([]);
+    return [];
   } else if (length % 2 === 1) {
-    return console.log([arr[Math.floor(length / 2)]]);
+    return [arr[Math.floor(length / 2)]];
   } else {
     const middleLeft = arr[length / 2 - 1];
     const middleRight = arr[length / 2];
-    return console.log([middleLeft, middleRight]);
+    return [middleLeft, middleRight];
   }
 }
 

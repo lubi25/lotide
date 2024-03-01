c471b622cda3710906aef35e5769b0d04ebe6a30
Lubi Islam <lubaba.islam91@gmail.com>
2024-02-29 23:32:05 -0500

    Remove unused require statements and commented out code

--- map.js
+++ map.js
@@ -1,11 +1,3 @@
-const assertEqual = require('./assertEqual');
-
-const assertArraysEqual = require('./assertArraysEqual');
-
-const eqArrays = require('./eqArrays');
-
-// const words = ["ground", "control", "to", "major", "tom"];
-
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
@@ -13,16 +5,5 @@ const map = function(array, callback) {
   }
   return results;
 }
-
-// const results1 = map(words, word => word[0]);
-// console.log(results1);
-
-// const results2 = map(words, word => word[2]);
-// console.log(results2);
-
-// const results3 = map(words, word => word.length);
-// console.log(results3);
-
-// assertArraysEqual(results1, ['g', 'c', 't', 'm', 't'])
 
 module.exports = map;

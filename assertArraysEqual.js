db9eaa153fe7cb6117a095b90f79752c21b00dba
Lubi Islam <lubaba.islam91@gmail.com>
2024-02-29 22:49:38 -0500

    Implemented eqArrays to assert if arrays are equal

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,13 +1,11 @@
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return console.log("false");
+  if (eqArrays(array1, array2)) {
+    console.log("true");
+  } else {
+    console.log("false");
   }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return console.log("false");
-    }
-  }
-  return console.log("true");
 };
 
 module.exports = assertArraysEqual;

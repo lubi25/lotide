1b87f152c8c8690e2296e521bc6104db79e21b79
Lubi Islam <lubaba.islam91@gmail.com>
2024-02-29 23:21:02 -0500

    Update value names and remove unused require statement

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,25 +1,10 @@
-const assertEqual = require('./assertEqual');
-
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-const findKeyByValue = function(show, genre) {
-  for (const key in show) {
-    if (show[key] === genre) {
+const findKeyByValue = function(theKey, theValue) {
+  for (const key in theKey) {
+    if (theKey[key] === theValue) {
       return key;
     }
   }
   return undefined;
 };
-
-// console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse"));
-
-// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), undefined);
-// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-
 
 module.exports = findKeyByValue;

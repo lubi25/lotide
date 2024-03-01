1e6e7d08dd116925457e086b5333d2a8518949ea
Lubi Islam <lubaba.islam91@gmail.com>
2024-02-29 23:20:36 -0500

    Remove unused require statement

--- findKey.js
+++ findKey.js
@@ -1,5 +1,3 @@
-const assertEqual = require('./assertEqual');
-
 const findKey = function(object, callback) {
   for (const key in object) {
     if (callback(object[key])) {
@@ -8,14 +6,5 @@ const findKey = function(object, callback) {
   }
   return undefined;
 }
-
-// console.log(findKey({
-//   "Blue Hill": { stars: 1 },
-//   "Akaleri":   { stars: 3 },
-//   "noma":      { stars: 2 },
-//   "elBulli":   { stars: 3 },
-//   "Ora":       { stars: 2 },
-//   "Akelarre":  { stars: 3 }
-// }, x => x.stars === 2)) // => "noma"
 
 module.exports = findKey;

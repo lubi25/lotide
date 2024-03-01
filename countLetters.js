530e7dee9fa27a82959a4c6f2a782ad8a3c21784
Lubi Islam <lubaba.islam91@gmail.com>
2024-02-29 23:19:13 -0500

    Remove unused require statement

--- countLetters.js
+++ countLetters.js
@@ -1,5 +1,3 @@
-const assertEqual = require('./assertEqual');
-
 function countLetters(allLetters) {
   var letCount = {};
 
@@ -16,18 +14,4 @@ function countLetters(allLetters) {
   return letCount;
 }
 
-// const result = countLetters("lighthouse in the house");
-// console.log(result);
-
-// assertEqual(result['l'], 1);
-// assertEqual(result['i'], 2);
-// assertEqual(result['g'], 1);
-// assertEqual(result['h'], 4);
-// assertEqual(result['t'], 2);
-// assertEqual(result['o'], 2);
-// assertEqual(result['u'], 2);
-// assertEqual(result['s'], 2);
-// assertEqual(result['e'], 3);
-// assertEqual(result['n'], 1);
-
-module.exports = assertEqual;
+module.exports = countLetters;

27fdcb763055de034b5dbc1e846ed5623efb3764
Lubi Islam <lubaba.islam91@gmail.com>
2024-03-01 00:06:05 -0500

    Update with recursion so that function returns an array

--- flatten.js
+++ flatten.js
@@ -1,5 +1,15 @@
-const flatten = function(arr) {
-  return console.log(arr.flat().join(', '));
-};
+const flatten = function(array) {
+  const flattened = [];
+
+  for (const element of array) {
+    if (Array.isArray(element)) {
+      flattened.push(...flatten(element));
+    } else {
+      flattened.push(element);
+    }
+  }
+
+  return flattened;
+}
 
 module.exports = flatten;

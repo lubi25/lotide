e59b74a04ff96690d3e617def83f5342a9c49107
Lubi Islam <lubaba.islam91@gmail.com>
2024-02-29 23:30:40 -0500

    Remove unused require statements and commented out code

--- flatten.js
+++ flatten.js
@@ -1,11 +1,5 @@
-const assertArraysEqual = require('./assertArraysEqual');
-
-const eqArrays = require('./eqArrays');
-
 const flatten = function(arr) {
   return console.log(arr.flat().join(', '));
 };
-
-// flatten([1, 2, [3, 4], 5, [6]])
 
 module.exports = flatten;
--- reverse.js
+++
@@ -1,8 +0,0 @@
-const args = process.argv.slice(2);
-args.forEach(str => {
-  let reversed = '';
-  for (let i = str.length - 1; i >= 0; i--) {
-    reversed += str[i];
-  }
-  console.log(reversed);
-});

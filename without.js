bfbb76e901e152bbb5856fed040c898499e20084
Lubi Islam <lubaba.islam91@gmail.com>
2024-02-29 23:33:03 -0500

    Remove unused require statements and commented out code

--- without.js
+++ without.js
@@ -1,7 +1,3 @@
-const assertArraysEqual = require('./assertArraysEqual');
-
-const eqArrays = require('./eqArrays');
-
 const without = function(source, itemsToRemove) {
   let remaining = [];
   for (let i = 0; i < source.length; i++) {
@@ -11,13 +7,5 @@ const without = function(source, itemsToRemove) {
   }
   return console.log(remaining);
 }
-
-// without([1, 2, 3], [1]) // => [2, 3]
-// without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
-
-// const words = ["hello", "world", "lighthouse"];
-// without(words, ["lighthouse"]); // no need to capture return value for this test case
-// // Make sure the original array was not altered by the without function
-// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
 
 module.exports = without;
